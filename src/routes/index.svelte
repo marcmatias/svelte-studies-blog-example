294bff327b22aa81e56bf14e5965a0a543e0fe2f
Marcel Marques <marcmatiasdev@gmail.com>
2021-10-24 22:17:23 -0300

    first commit

---
+++ src/routes/index.svelte
@@ -0,0 +1,5 @@
+<h1>Homepage</h1>
+
+Lorem ipsum dolor sit amet consectetur adipisicing elit. Obcaecati accusantium
+ipsum itaque ipsam tempora aperiam corrupti saepe eius possimus, commodi dolorem
+doloremque inventore sapiente incidunt explicabo expedita porro fugit? Id.
